6e2bf4d8cba59a01cf420f0e42ba0adb75656bd4
shippo <297029445@qq.com>
2017-01-04 09:35:36 +0800

    if +

---
+++ ex31.py
@@ -0,0 +1,33 @@
+print("You enter a dark room with 2 doors. ",
+"Do you go through door1 or door2?")
+
+door = input(">")
+
+if door == "1":
+	print("There is a giant bear eating a cheese cake. What do you do?")
+	print("1. Take the cake.")
+	print("2. Scream at the bear.")
+	
+	bear = input(">")
+	
+	if bear == "1":
+		print("The bear eats your face off. Good job.")
+		
+	elif bear == "2":
+		print("The bear eats your leg off. Good job.")
+	else:
+		print("Well, doing %s if probably better. Bear runs away." % bear)
+		
+elif door == "2":
+	print("You stare into the endless abyss at Cthulhu's retina.")	
+	print("1. Blueberries.")
+	print("2. Yello jacket clothespins.")
+	print("3. Understanding revolvers yelling mmelodies.")
+	
+	insanity = input(">")
+	if insanity == "1" or insanity == "2":
+		print("Your body survives powered by a mind of jello. Good job!")
+	else:
+		print("The insanity rots your eyes into a pool of muck. Good job!")
+else:
+		print("You stumble around and fall on a knife and die. Good job!")	
